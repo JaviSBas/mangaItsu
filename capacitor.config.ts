35b5e5b6dea8b3ed3edf222c58cb5c702ebe5d2c
Javi Sbass <javi.sbass@gmail.com>
2023-08-27 16:39:42 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'listadoManga',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
